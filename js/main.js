aca61fc406c2f0c0aebef53147cf7b34d978003e
José López G <antares069@gmail.com>
2022-01-19 21:05:56 -0600

    refactorizar

--- js/main.js
+++ js/main.js
@@ -21,6 +21,54 @@ function calcularAreaCuadrado(){
     createElemento(areaCuadrado, sectionCuadrado, text);
 };
 
+function calcularPerimetroTriangulo(){
+    const TrianguloLadoDerecho = document.getElementById("InputTrianguloLadoDerecho");
+    const TrianguloLadoIzquierdo = document.getElementById("InputTrianguloLadoIzquierdo");
+    const TrianguloBase = document.getElementById("InputTrianguloBase");
+
+    let perimetroTriangulo = parseFloat(TrianguloBase.value) + parseFloat(TrianguloLadoDerecho.value) + parseFloat(TrianguloLadoIzquierdo.value);
+    let sectionTriangulo = document.getElementById("formTriangulo");
+    let text = " Perimetro de tu Triangulo";
+
+    createElemento(perimetroTriangulo, sectionTriangulo, text);
+}
+
+function calcularAreaTriangulo(){
+    const trianguloBase = document.getElementById("InputTrianguloBase").value;
+    const trianguloAltura = document.getElementById("InputTrianguloAltura").value;
+
+    let areaTriangulo = (trianguloBase * trianguloAltura)/2;
+    let sectionTriangulo = document.getElementById("formTriangulo");
+    let text = " Area de tu Triangulo";
+
+    createElemento(areaTriangulo, sectionTriangulo, text);
+}
+
+function calcularPerimetroCirculo(){
+    const radioCirculo = document.getElementById("inputRadioCirculo").value;
+    const pi = Math.PI;
+    
+    let perimetroCirculoPre = pi * radioCirculo * 2;
+    let perimetroCirculo = perimetroCirculoPre.toFixed(2);
+    let sectionCirculo = document.getElementById("formCirculo");
+    let text = " Perimetro de tu Circulo";
+
+    createElemento(perimetroCirculo, sectionCirculo, text);
+}
+
+function calcularAreaCirculo(){
+    const radioCirculo = document.getElementById("inputRadioCirculo").value;
+    const pi = Math.PI;
+    
+    let areaCirculoP = pi * radioCirculo * radioCirculo;
+    let areaCirculo = areaCirculoP.toFixed(2);
+
+    let sectionCirculo = document.getElementById("formCirculo");
+    let text = " Area de tu Circulo";
+
+    createElemento(areaCirculo, sectionCirculo, text);
+}
+
 function createElemento(value, section, text){
     const parrafo = document.createElement("p");
     parrafo.setAttribute("id", "parrafo");
@@ -32,56 +80,4 @@ function createElemento(value, section, text){
         section.removeChild(document.getElementById("parrafo"));
         section.appendChild(parrafo);
     }
-}
-
-function calcularPerimetroTriangulo(){
-    const TrianguloLadoDerecho = document.getElementById("InputTrianguloLadoDerecho");
-    const TrianguloLadoIzquierdo = document.getElementById("InputTrianguloLadoIzquierdo");
-    const TrianguloBase = document.getElementById("InputTrianguloBase");
-
-    let perimetroTriangulo = parseFloat(TrianguloBase.value) + parseFloat(TrianguloLadoDerecho.value) + parseFloat(TrianguloLadoIzquierdo.value);
-
-    const parrafo = document.createElement("p");
-	parrafo.textContent =`El valor del perímetro de tu triangulo es de: ${perimetroTriangulo} cm.`;
-    let sectionCuadrado = document.getElementById("sectionTriangulo");
-    sectionCuadrado.appendChild(parrafo);
-}
-
-function calcularAreaTriangulo(){
-    const trianguloBase = document.getElementById("InputTrianguloBase").value;
-    const trianguloAltura = document.getElementById("InputTrianguloAltura").value;
-
-    let areaTriangulo = (trianguloBase * trianguloAltura)/2;
-    console.log(areaTriangulo);
-
-    const parrafo = document.createElement("p");
-	parrafo.textContent =`El valor del área de tu triangulo es de: ${areaTriangulo} cm.`;
-    let sectionCuadrado = document.getElementById("sectionTriangulo");
-    sectionCuadrado.appendChild(parrafo);
-}
-
-function calcularPerimetroCirculo(){
-    const radioCirculo = document.getElementById("inputRadioCirculo").value;
-    const pi = Math.PI;
-    
-    let perimetroCirculoPre = pi * radioCirculo * 2;
-    let perimetroCirculo = perimetroCirculoPre.toFixed(2);
-
-    const parrafo = document.createElement("p");
-	parrafo.textContent =`El valor del perímetro de tu triangulo es de: ${perimetroCirculo} cm.`;
-    let sectionCuadrado = document.getElementById("sectionCirculo");
-    sectionCuadrado.appendChild(parrafo);
-}
-
-function calcularAreaCirculo(){
-    const radioCirculo = document.getElementById("inputRadioCirculo").value;
-    const pi = Math.PI;
-    
-    let areaCirculoP = pi * radioCirculo * radioCirculo;
-    let areaCirculo = areaCirculoP.toFixed(2);
-
-    const parrafo = document.createElement("p");
-	parrafo.textContent =`El valor del área de tu Circulo es de: ${areaCirculo} cm.`;
-    let sectionCuadrado = document.getElementById("sectionCirculo");
-    sectionCuadrado.appendChild(parrafo);
 }
